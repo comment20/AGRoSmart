d2e42757d31bf5c5370918fd6c9ae17fd81258e1
boval germain tchoupe <bovalgermaintchoupe@.com>
2026-05-20 08:53:47 +0200

    reparation du lienr

--- SoilClassifier/settings.py
+++ SoilClassifier/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-placeholder-for-local-dev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.56.1', '.render.com']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
         'http://127.0.0.1:8000',
